c2899ba378afc05ece8566d89dd9dc5d701203c4
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-04-29 23:14:12 -0500

    worked on first part

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -110,7 +110,15 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+//for of loop, for index loop, getKey(), getValue()
 
+//        ArrayList<HashMap<String, String>> jobArray = new ArrayList<>();
+
+//        for (HashMap result : results) {
+//            for (int i = 0; i < results.size(); i++) {
+//                jobArray.add(result);
+//            }
+//        }
         System.out.println("printJobs is not implemented yet");
     }
 }
